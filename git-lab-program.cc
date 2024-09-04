955db8ce1db7b16adb8927eb31c9aab6d39aa0a8
rileymelchi <rm427522@ohio.edu>
2024-09-04 12:31:39 -0400

    Code2

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Riley Melchi
+ *        Date: 9/4/24
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello git!!" << endl;
+    return 0;
+}// main
